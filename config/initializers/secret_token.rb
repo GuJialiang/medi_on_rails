cb488e3c0076adef5c205a05e51b18c2eca6289a
Jialiang Gu <jgu1@andrew.cmu.edu>
2012-09-25 09:19:52 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MediOnRails::Application.config.secret_token = 'ff7b4e84b63e0a15644d88f1b9239a729531f995e06294b25bb96c85c71e791feb54cb749be730f21c473980f96e4618c1f65b3966035bbde4be11abc24fb97d'
